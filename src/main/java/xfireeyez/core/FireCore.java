10854a0ba5ba919b0f6e6dc7874eea6aec41c39f
XFireEyeZ <h.filip.holmberg@gmail.com>
2017-06-26 11:37:19 +0200

    Fixes
    Fixed some stuff

--- src/main/java/xfireeyez/core/FireCore.java
+++ src/main/java/xfireeyez/core/FireCore.java
@@ -2,13 +2,20 @@ package xfireeyez.core;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import xfireeyez.core.proxy.IProxy;
 import xfireeyez.core.util.Utils;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class FireCore {
+	
+	@Mod.Instance(Reference.MODID)
+	public static FireCore instance = new FireCore();
+	@SidedProxy(serverSide = Reference.SERVER_PROXY_CLASS, clientSide = Reference.CLIENT_PROXY_CLASS)
+	public static IProxy proxy;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
